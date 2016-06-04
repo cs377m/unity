384abea63ffedd7a639e46baf353f1d61385b192
Jennifer <jenylu@stanford.edu>
2016-06-04 16:14:22 -0700

    added framework for changing movements with an int (similar to heartbeat). added glow when butterflies  are close to finger. Calibrated butterflies so in about the right place for user to reach. removed blue made with holo lens-whatever screen. did minor debugging and now setting position and scale for butterfly instead of its parent (thats  why scaling was weird before - parent was about 2 units away from the animated butterfly, so we scaled the distance be tween the two which resulted in the strange effect where it looked like butterflies moved far away). Hooked in heartbea t so it changes the period of movement inversely

--- Assets/Scripts/ButterflyPrefabPopulator.cs
+++ Assets/Scripts/ButterflyPrefabPopulator.cs
@@ -11,11 +11,11 @@ public class ButterflyPrefabPopulator : MonoBehaviour {
     public Vector3 originButterflyCloud = new Vector3(0, -1, 8);
     public static int nButterflies = 30;
     public int minX = 0;
-    public int maxX = 4;
-    public int minY = -3;
-    public int maxY = 3;
+    public int maxX = 1;
+    public int minY = -1;
+    public int maxY = 1;
     public int minZ = 2;
-    public int maxZ = 8;
+    public int maxZ = 4;
     System.Random rnd = new System.Random();
 
 
@@ -34,20 +34,16 @@ public class ButterflyPrefabPopulator : MonoBehaviour {
     private Color highlightColor = new Color(255, 0, 0, 1);
 
     private int heartRate;
+    private int movementMode = 2; // THIS CONTROLS WHICH movement code we use below (1=A, 2=B, ...)
 
     private long updateIter = 0;
 
+    private float glowDistance = 0.9f; // The distance a user's hand has to be from a butterfly to make it glow
 
-    //private TrackableBehaviour mTrackableBehavior;
+
 
     // Use this for initialization
     void Start() {
-        //mTrackableBehavior = GetComponent<TrackableBehaviour>();
-
-        //if (mTrackableBehavior)
-        //{
-        //    mTrackableBehavior.RegisterTrackableEventHandler(this);
-        //}
         InstantiateButterflies();        
     }
 
@@ -56,13 +52,12 @@ public class ButterflyPrefabPopulator : MonoBehaviour {
         for (int i = 0; i < nButterflies; i++)
         {
             butterflies[i] = (GameObject)Instantiate(prefab, originButterflyCloud, Quaternion.identity);
+            Behaviour halo = (Behaviour)butterflies[i].GetComponent("Halo");
+            Behaviour innerHalo = (Behaviour)butterflies[i].transform.GetChild(0).gameObject.GetComponent("Halo");
+            halo.enabled = false;
+            innerHalo.enabled = false;
 
-            //if (mTrackableBehavior.transform)
-            //{
-            //    butterflies[i].transform.parent = mTrackableBehavior.transform;
-            //}
-
-            butterflies[i].transform.position = new Vector3(0.001f*i*rnd.Next(-5,5), 0.001f*i*rnd.Next(-5,5), -0.001f*i*rnd.Next(-5,5));
+            //butterflies[i].transform.position = new Vector3(0.001f*i*rnd.Next(-5,5), 0.001f*i*rnd.Next(-5,5), -0.001f*i*rnd.Next(-5,5));
 
             int x = rnd.Next(minX, maxX);
             int y = rnd.Next(minY, maxY);
@@ -70,6 +65,7 @@ public class ButterflyPrefabPopulator : MonoBehaviour {
             m_Pivot[i] = new Vector3(x, y, z);
             m_Phase[i] = rnd.Next(0, 6);
             m_Invert[i] = m_Phase[i] > 3;
+            butterflies[i].transform.position = m_Pivot[i];
         }
     }
 
@@ -94,19 +90,52 @@ public class ButterflyPrefabPopulator : MonoBehaviour {
 
     Vector3 movementA(long iter)
     {
-        return new Vector3(0, 0.001f, 0);
+        //double periodInIter = heartRate
+        double periodInIter = 100;
+        double amplitude = 0.08;
+        double movement = amplitude*Math.Sin(2 * Math.PI * iter / periodInIter);
+        return new Vector3(0, (float)movement, 0);
     }
 
     Vector3 movementB(long iter)
     {
-        if (iter < 10000) return new Vector3(0, 0, 0.01f);
-        else return new Vector3(0, 0.01f, 0);
+        //double periodInIter = heartRate
+        double periodInIter = 100;
+        double amplitude = 0.08;
+        double movement = amplitude * Math.Sin(2 * Math.PI * iter / periodInIter);
+        double xMovement = amplitude * 0.5 * Math.Cos(2 * Math.PI * iter / periodInIter);
+        return new Vector3((float)xMovement, (float)movement, 0);
     }
 
     Vector3 movementC(long iter)
     {
-        if (iter < 10000) return new Vector3(0, 0, 0.01f);
-        else return new Vector3(0, 0.01f, 0);
+        //double periodInIter = heartRate
+        int periodInIter = 100;
+        double pseudoPhase = ((int)iter % periodInIter)/(double)periodInIter;
+        double amplitude = 0.08;
+
+        double xMovement = 0;
+        double yMovement = 0;
+        double zMovement = 0;
+
+        if (pseudoPhase < 0.5)
+        {
+            xMovement = amplitude * 0.4 * Math.Cos(2 * Math.PI * pseudoPhase);
+            yMovement = amplitude * Math.Cos(2 * Math.PI * pseudoPhase);
+            zMovement = amplitude * 0.2 * Math.Cos(2 * Math.PI * pseudoPhase);
+        } else if (pseudoPhase < 0.75)
+        {
+            xMovement = 0;
+            yMovement = 0;
+            zMovement = amplitude * 0.1;
+        } else
+        {
+            xMovement = 0;
+            yMovement = 0;
+            zMovement = amplitude * -0.1;
+        }
+
+        return new Vector3((float)xMovement, (float)yMovement, (float)zMovement);
     }
 
     Vector3 movementD(long iter)
@@ -117,8 +146,40 @@ public class ButterflyPrefabPopulator : MonoBehaviour {
 
     Vector3 movementE(long iter)
     {
+        //m_PivotOffset[i] = Vector3.up * 2 * m_YScale;
+
+        //m_Phase[i] += m_Speed * Time.deltaTime;
+        //if (m_Phase[i] > m_2PI)
+        //{
+        //    m_Invert[i] = !m_Invert[i];
+        //    m_Phase[i] -= m_2PI;
+        //}
+        //if (m_Phase[i] < 0) m_Phase[i] += m_2PI;
+
+        //butterflies[i].transform.position = m_Pivot[i] + (m_Invert[i] ? m_PivotOffset[i] : Vector3.zero);
+        //butterflies[i].transform.position = butterflies[i].transform.position +
+        //    new Vector3(Mathf.Sin(m_Phase[i]) * m_XScale,
+        //                Mathf.Cos(m_Phase[i]) * (m_Invert[i] ? -1 : 1) * m_YScale,
+        //                Mathf.Sin(m_Phase[i]*3) * m_ZScale);
         if (iter < 10000) return new Vector3(0, 0, 0.01f);
         else return new Vector3(0, 0.01f, 0);
+    }
+
+    private Vector3 getMovementIncrement(long updateIter)
+    {
+        switch (movementMode)
+        {
+            case 1:
+                return movementA(updateIter);
+            case 2:
+                return movementB(updateIter);
+            case 3:
+                return movementC(updateIter);
+            case 4:
+                return movementD(updateIter);
+            default:
+                return movementE(updateIter);
+        }
     }
 
 
@@ -137,14 +198,20 @@ public class ButterflyPrefabPopulator : MonoBehaviour {
 
         Debug.Log("CURRENT HAND POSITION: " + currentHandPosition.ToString()); 
 
-
+        
         for (int i = 0; i < nButterflies; i++)
         {
-            
-            if (Vector3.Distance(currentHandPosition, butterflies[i].transform.position) < 3 && Vector3.Distance(currentHandPosition, new Vector3(0,0,0)) > 0) 
+            // This is the object that corresponds to the "real" butterfly, not the 
+            // parent container object
+            GameObject realButterfly = butterflies[i].transform.GetChild(0).gameObject;
+            Behaviour halo = (Behaviour)realButterfly.GetComponent("Halo");
+            if (Vector3.Distance(currentHandPosition, butterflies[i].transform.position) < glowDistance && Vector3.Distance(currentHandPosition, new Vector3(0,0,0)) > 0) 
             {
                 // CHANGE BUTTERFLY SOMEHOW
-                butterflies[i].transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+                //butterflies[i].transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+                realButterfly.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+
+                halo.enabled = true;
 
                 Debug.Log("HAND POS: " + currentHandPosition.ToString());
                 Debug.Log("BUTTERFLY POS: " + butterflies[i].transform.position.ToString());
@@ -152,25 +219,21 @@ public class ButterflyPrefabPopulator : MonoBehaviour {
             }
             else
             {
-                butterflies[i].transform.localScale = new Vector3(1, 1, 1);
+                //butterflies[i].transform.localScale = new Vector3(1, 1, 1);
+                realButterfly.transform.localScale = new Vector3(1f, 1f, 1f);
+                halo.enabled = false;
             }
 
+            //m_PivotOffset[i] = Vector3.up * 2 * m_YScale;
 
-            m_PivotOffset[i] = Vector3.up * 2 * m_YScale;
+            //m_Phase[i] += m_Speed * Time.deltaTime;
+            //if (m_Phase[i] > m_2PI)
+            //{
+            //    m_Invert[i] = !m_Invert[i];
+            //    m_Phase[i] -= m_2PI;
+            //}
+            //if (m_Phase[i] < 0) m_Phase[i] += m_2PI;
 
-            m_Phase[i] += m_Speed * Time.deltaTime;
-            if (m_Phase[i] > m_2PI)
-            {
-                m_Invert[i] = !m_Invert[i];
-                m_Phase[i] -= m_2PI;
-            }
-            if (m_Phase[i] < 0) m_Phase[i] += m_2PI;
-
-            // Call Team PJE's custom movement function for the butterfly.
-            Vector3 movementTrajectory = movementA(updateIter);
-            butterflies[i].transform.position = butterflies[i].transform.position + movementTrajectory;
-            
-            
             //butterflies[i].transform.position = m_Pivot[i] + (m_Invert[i] ? m_PivotOffset[i] : Vector3.zero);
             //butterflies[i].transform.position = butterflies[i].transform.position +
             //    new Vector3(Mathf.Sin(m_Phase[i]) * m_XScale,
@@ -178,30 +241,17 @@ public class ButterflyPrefabPopulator : MonoBehaviour {
             //                Mathf.Sin(m_Phase[i]*3) * m_ZScale);
 
 
+            // Call Team PJE's custom movement function for the butterfly.
+            //Vector3 movementTrajectory = movementA(updateIter);
+            Vector3 movementTrajectory = getMovementIncrement(updateIter);
+            butterflies[i].transform.position = butterflies[i].transform.position + movementTrajectory;
+            
+            
+            
+
+
             
         }
 	}
 
-//    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus,
-//                                       TrackableBehaviour.Status newStatus)
-//    {
-//        Debug.Log("TRACKABLE STATE CHANGED!");
-//        if (newStatus == TrackableBehaviour.Status.DETECTED) Debug.Log("DETECTED");
-//        if (newStatus == TrackableBehaviour.Status.TRACKED) Debug.Log("TRACKED");
-//        if (newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED) Debug.Log("EXTENDED TRACKED");
-
-//        if (newStatus == TrackableBehaviour.Status.DETECTED ||
-//            newStatus == TrackableBehaviour.Status.TRACKED ||
-//            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
-//        {
-//            OnTrackingFound();
-//        }
-//    }
-
-//    private void OnTrackingFound()
-//    {
-//        //InstantiateButterflies();
-
-//        Debug.Log("TRAVCKING FOUND");
-//    }
 }
